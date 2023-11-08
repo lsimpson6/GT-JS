913b597464acce3bc72271523bb700137ae40fd5
Logan Simpson <82660175+lsimpson6@users.noreply.github.com>
2023-11-08 09:21:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,12 +9,10 @@ document.querySelectorAll('.cta-button').forEach(cta => cta.addEventListener('cl
 }));
 
 window.addEventListener('load', ()=>{
-	console.log('main load');
 	let windowUrl = window.location.search;
 	let params = new URLSearchParams(windowUrl);
         document.querySelectorAll('.category-title').forEach(t => {el++; t.setAttribute("data-hyperlink-number", el)})
   	if(params.has('subId') && params.get('subId') != "" || params.get('subId') != null){
-		console.log('sub id is true, run');
 		let nameField = document.getElementById('on_behalf_of-bf0c727e-e018-4107-bc9b-a4480dd1f5af');
 		let nameData = document.getElementById('thankYouModal');
 		nameField.textContent = nameData.getAttribute('data-fname') + ' ' + nameData.getAttribute('data-lname');
@@ -22,15 +20,12 @@ window.addEventListener('load', ()=>{
 	        document.getElementById('thankyou-modal-button').click();
 	        document.getElementById('header-main').setAttribute('data-modal-was-shown', "true");
   	}else{
-	setInterval(()=>{;
-	    i++;
-		console.log('interval' + i);
-	    if(i > 249 && document.getElementById('header-main').getAttribute('data-modal-was-shown') == "false"){
-		console.log('set to true within interval');
+	setTimeout(()=>{;
+	    if(document.getElementById('header-main').getAttribute('data-modal-was-shown') == "false"){
 		document.getElementById('suggestion-modal-button').click();
 		document.getElementById('header-main').setAttribute('data-modal-was-shown', "true");
 	    }
-	}, 100)
+	}, 25000)
 	}
 })
  
